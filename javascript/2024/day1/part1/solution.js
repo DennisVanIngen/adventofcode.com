482bcbe3af2a7c1f8ce03470ef236d2109c40c8e
dennisvaningen <dennis@dennisvaningen.nl>
2024-12-07 19:49:57 +0100

    Day 1 part 1

---
+++ javascript/2024/day1/part1/solution.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+import readline from 'readline';
+
+let counter = 0;
+const leftSide = [];
+const rightSide = [];
+
+const rd = readline.createInterface({
+    input: fs.createReadStream('javascript/2024/day1/part1/input.txt'),
+    console: false
+});
+
+rd.on('line', function (line) {
+    const [, leftNumber, rightNumber] = line.match(/(\d+)\s+(\d+)/);
+    leftSide.push(parseInt(leftNumber));
+    rightSide.push(parseInt(rightNumber));
+});
+
+rd.on('close', function () {
+    leftSide.sort((a, b) => a - b);
+    rightSide.sort((a, b) => a - b);
+    for (let i = 0; i < leftSide.length; i++) {
+        counter += Math.abs(leftSide[i] - rightSide[i]);
+    }
+    console.log("Total difference: " + counter);
+});
